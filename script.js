2682008cbcb5bc37898747c07a81b16d5883f08b
tomar-ai <ankittomar.nl@gmail.com>
2024-08-24 23:20:15 +0200

    updated js

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ console.log(a,b,c);
 const [x,y,z]= arr; // destructing the array 
 console.log(x,y,z)
 console.log(arr)
+
+console.log(arr[2])
+
+// swapping the arry 
+
+const reasturant = {
+    name : "ankit",
+    age: 30,
+    places : ["Itlay", "AMS", "Seattle"],
+    mainMenu : ["Pizza", "tea"]
+
+}
+
+
+// value swapping
+let [main, secondary] =  reasturant.mainMenu;
+
+console.log(main, secondary)
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// console.log(main, secondary) // we had to use let insted of const 
+
+// [main, secondary] = [secondary, main];
+
+// console.log(main, secondary);
+
+
+
+// nested arry 
+const nestArray = [2,3, [4,5]]
+console.log(nestArray)
+
+const [i, j, [k,l]] = nestArray;
+console.log(i,j,k,l)
+
+
